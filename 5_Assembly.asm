23647b4a79ac11523363bff3c8476dcd232ec586
Júlia Bertolini <juliadarosabertolini@gmail.com>
2026-09-10 11:41:38 -0300

    Update 5_Assembly.asm

--- 5_Assembly.asm
+++ 5_Assembly.asm
@@ -1,36 +1,95 @@
+section .data
+    msg_1 db "Digite o N1: "
+    len_msg1 equ $-msg_1
+    
+    msg_2 db "Digite o N2: "
+    len_msg2 equ $-msg_2
+    
+    msg_3 db "Digite a Operacao (+, -, *, /): "
+    len_msg3 equ $-msg_3
+    
+    quebra db 0xa
+    
 section .bss
-	valor resb 1
-
+    n1 resb 2
+    n2 resb 2
+    char resb 2
+    resultado resb 2
+    
 section .text
-	global _start
-
+    global _start
+    
 _start:
-	 mov esi, 0
+;print msg 1
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, msg_1
+    mov edx, len_msg1
+    
+    int 0x80
+;ler n1    
+    mov eax, 3
+    mov ebx, 0 
+    mov ecx, n1
+    mov edx, 1 
+    
+    int 80h
+    
+;consumir o enter
+    mov eax, 3 
+    mov ebx, 0 
+    mov ecx, char
+    mov edx, 1 
+    int 80h
+    
+;quebra
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, quebra
+    mov edx, 1
+  
+    int 80h
+    
+;print msg 2    
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, msg_2
+    mov edx, len_msg2
+    
+    int 0x80
+    
+; ler n2    
+    mov eax, 3
+    mov ebx, 0 
+    mov ecx, n2
+    mov edx, 1 
+    
+    int 80h
 
-inicio_loop:
-	cmp esi, 5
-	je fim_loop
-
-;========================
-	mov edi, esi
-	add edi, '0'
-	mov[valor], edi
-
-;PRINTANDO O VALOR DO CONTADOR
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, valor
-	mov edx, 1
-
-	int 80h
-;========================
-
-	inc esi 
-
-	jmp inicio_loop
-
-fim_loop:
-	mov eax, 1
-	mov ebx, 0
-
-	int 0x80
+;consumir o enter
+    mov eax, 3 
+    mov ebx, 0 
+    mov ecx, char 
+    mov edx, 1
+    
+;print n2
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, n2
+    mov edx, 1 
+    
+    int 80h
+    
+;print n1
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, n1
+    mov edx, 1
+    
+    int 80h
+    
+Fim_programa:
+    mov eax, 1
+    mov ebx, 0
+    
+    int 0x80
